d8c00f50e1ff196abe3c3fe837aa96e493a6b06b
Gv1n4ich <besk-zed@mail.ru>
2026-08-26 18:54:58 +0300

    chore: initialize DevMatch project

---
+++ matching/src/main/java/com/match/matching/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.match.matching.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HealthController {
+    @GetMapping("/api/health")
+    public String health() {
+        return "DevMatch is running!";
+    }
+}
